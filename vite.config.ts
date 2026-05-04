8ba65a777027cfa3e68ffa19b23010853ddb7f27
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2026-05-04 16:51:30 -0400

    fix repo name

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/xcimer-slides/',  // my repo name
+  base: '/fuel-cycle/',  // my repo name
 })
